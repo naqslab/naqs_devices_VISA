8f4c439fc3e947137d116c81218bf003c80978fa
Jason Pruitt <55477390+Json-To-String@users.noreply.github.com>
2025-06-09 15:59:14 -0400

    old comments + forgot an underscore

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'naqs_devices VISA'
+project = 'naqs_devices_VISA'
 copyright = '2025, naqslab'
 author = 'naqslab'
 
